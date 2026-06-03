3c1e02058abd9981a3404a8bf1843c8ae13c0d19
Jelle Thijs <jellesieme2003@gmail.com>
2026-06-03 10:47:44 +0200

    Introduceer de mogelijk voor meerdere leds aan
    - verduidelijk het commentaar bij de pins array.
    - haal gelijk al 1 van de pins array af.
    - voeg een extra variabel toe om aan te geven hoeveel lampjes er extra moeten branden.
    - voeg een variabel voor de grote van de loop toe.
    - update de for loops met de nieuwe variabel
    - gebruik if statements op binnen de pins array te blijven
    - voeg commentaar toe voor de verduidelijking van de code

--- Opdracht 2/Code_Opdracht_2.ino
+++ Opdracht 2/Code_Opdracht_2.ino
@@ -1,32 +1,45 @@
 // defineert welke pinnen er worden gebruikt.
 int pins[] = {2,3,4,5,6,7,8,9}; 
 
-// berekend de groote van de pins array.
-int pins_size = sizeof(pins) / sizeof(pins[0]); 
+// berekend de groote van de pins array. 
+// min 1 omdat arrays starten bij 1.
+int pins_size = sizeof(pins) / sizeof(pins[0]) - 1;
+
+// defineert hoeveel pinnen er extra moeten blijven branden 
+int extra = 3;
+
+// bereken de loop size door de grote van de array te berekenen
+// plus de extra waarde toe te voegen. 
+// min 1 aangezien de een array bij nul moet beginnen.
+int loop_size = pins_size + extra;
 
 void setup() {
   // zet alle pinnen naar output modus.
-  for(int i = 0; i <= pins_size - 1; i++) pinMode(pins[i], OUTPUT);
+  for(int i = 0; i <= pins_size; i++) pinMode(pins[i], OUTPUT);
 }
 
 void loop() {
   // ga door alle pinnen heen en zet deze voor 200 ms aan.
-  // sla de laatste pin over aangezien 
-  // die al aangaat in de loop hieronder.
-  for (int i = 0; i <= pins_size - 2; i++){
-    digitalWrite(pins[i], HIGH);
+  // laad een vooraf gedefineerd aantal pinnen aan staan.
+  for(int i = 0; i <= loop_size; i++){
+    // gebruik een if statement om binnen de pins array te blijven.
+    if(i <= pins_size) digitalWrite(pins[i], HIGH);
     delay(200);
-    digitalWrite(pins[i], LOW);
+    
+    // zet een pin uit die extra aantal terug is in de pins array.
+    if(i - extra >= 0) digitalWrite(pins[i - extra], LOW);
   }
   
   // ga in omgedraaide volgorde door alle pinnen heen
   // en zet deze voor 200 ms aan. 
-  // sla de eerste pin over aangezien 
-  // die al aangaat in de loop hierboven.
-  for (int i = pins_size - 1; i >= 1; i--){
-    digitalWrite(pins[i], HIGH);
+  // laad een vooraf gedefineerd aantal pinnen aan staan.
+  for(int i = loop_size; i >= 0; i--){
+    // zet een pin aan die extra aantal terug is in de pins array.
+    // array begint met een hoger nummer dan het aantal pins, 
+    // om te zorgen dat de pinnen later kunnen wordern uitgezet
+    if(i - extra >= 0) digitalWrite(pins[i - extra], HIGH);
     delay(200);
-    digitalWrite(pins[i], LOW);
+    
+    if(i <= pins_size) digitalWrite(pins[i], LOW);
   }
-
 }
