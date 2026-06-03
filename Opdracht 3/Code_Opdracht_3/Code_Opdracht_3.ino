ea1a44785ba6b6c3f14c29718832e655fcf98843
Jelle Thijs <jellesieme2003@gmail.com>
2026-06-03 11:00:04 +0200

    Schrijf de code voor opdracht 3 met commentaar
    - maak een array met de pinnen van de leds
    - bereken de groote van de array met sizeof. deze geeft de bytegrootte terug, dus daaom moet er nog worden gedeeld door de groote van de item. Er wordt ook gelijk 1 vanaf gehaalt aangezien arrays starten met 0.
    - maak een variabel van de pin van de knop
    - maak een variabel die bijhoud hoe vaak er op de knop is gedrukt
    - houd de vorige input bij, zodat de for loop niet blijft uitvoeren als de knop ingedrukt word gehouden.
    - zet de modus voor de pinnen
    - lees de knop
    - als op de knop wordt gedrukt gaat een led aan gebaseerd of dat pressed_counter + 1 te delen is door 2
    - als op de knop wordt gedrukt gaat een led uit gebaseerd of dat pressed_counter te delen is door 2

---
+++ Opdracht 3/Code_Opdracht_3/Code_Opdracht_3.ino
@@ -0,0 +1,41 @@
+// defineert de led pinnen.
+int led_pins[] = {8, 10}; 
+
+// berekend de groote van de pins array.
+int led_pins_size = sizeof(led_pins) / sizeof(led_pins[0]) - 1; 
+
+// defineert de knop pin.
+int btn_pin = 12;
+
+// houd bij hoe vaak de knop is ingedrukt.
+int pressed_counter = 0;
+
+// houd de laatste invoer bij. Staat alvast standaard op HIGH
+int last_input = HIGH;
+
+
+void setup()
+{
+  // zet de modus voor de pinnen.
+  for(int i = 0; i <= led_pins_size; i++) pinMode(led_pins[i], OUTPUT);
+  pinMode(btn_pin, INPUT);
+}
+
+void loop()
+{
+  // maak een variabel voor het bijhouden van de knop.
+  int current_input = digitalRead(btn_pin);
+  
+  // controleerd dat de knop is ingedrukt en dat de knop 
+  // niet al is ingedrukt.
+  if(current_input == HIGH && last_input == LOW) {
+    pressed_counter ++;
+    digitalWrite(led_pins[(pressed_counter + 1) % 2], HIGH);
+    digitalWrite(led_pins[pressed_counter % 2], LOW);
+    delay(50);
+  }
+  
+  // zet de laatste invoer naar de huidige invoer 
+  // om ingedrukt houden te ondersteunen. 
+  last_input = current_input;
+}
