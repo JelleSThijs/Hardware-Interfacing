f75b37f30b7ac36ef5167f6b6a979fcaaaf0cbe9
Jelle Thijs <jellesieme2003@gmail.com>
2026-06-04 13:35:27 +0200

    Schrijf de code voor opdracht 4 met commentaar
    - importeer de Servo.h studio.
    - maak een servo object aan genaamd servo.
    - stel variablen in voor de pinnen 6 en 7. Deze spreken de knoppen aan.
    - maak een array met 2 angles waar de servo tussen heen en weer moet gaan.
    - maak variablen voor de current angle en de target_angle. Deze worden gebruikt om te bepalen of de servo moet bewegen.
    - maak de ta_counter variable voor het wisselen tussen de target angles. hier wordt 1 bij opgeteld, zodra de servo de target angle heeft behaald.
    - zet de modus voor de knop pinnen.
    - stel pin 11 in voor het servo object.
    - beweeg de servo naar de start positie.
    - maak de control_servo functie waar twee delays kunnen worden meegegeven. Deze functie beweegt de servo naar rechts in de tijd van de eerste delay. Dan wordt de target angle om gewisseld. Vervolgens beweegd de servo terug naar links in de tijd van de tweede delay.
    - voer de control servo functie uit met een 5 seconden en 0,5 seconden delay zolang als allebij de knoppen worden ingedrukt.
    - voer de control servo functie uit met een 5 seconden en 5 seconden delay zolang alleen de eerste knop wordt ingedrukt.
    - voer de control servo functie uit met een 0,5 seconden en 0,5 seconden delay zolang als alleen de tweede knop wordt ingedrukt.

---
+++ Opdracht 5/Code_Opdracht_5/Code_Opdracht_5.ino
@@ -0,0 +1,93 @@
+#include <Servo.h>
+
+// maak een servo object aan genaamd servo.
+Servo servo;
+
+// stel de pinnen voor de knoppen in.
+const int btn_1 = 6;
+const int btn_2 = 7;
+
+// stel de hoeken in. 
+const int target_angles[] = {0, 120};
+
+// stel de variablen in die niet vast staan.
+int current_angle = 0;
+int target_angle = 120;
+int ta_counter = 1;
+
+void setup()
+{
+  // zet de modus van de knop pinnen naar input.
+  pinMode(btn_1, INPUT);
+  pinMode(btn_2, INPUT);
+  
+  // spreek de pin van de servo aan.
+  servo.attach(11);
+  
+  // zet de servo naar de eerste positie.
+  servo.write(current_angle);
+}
+
+void loop()
+{ 
+  // als beide knop zijn ingedrukt, wordt de control servo functie
+  // uitgevoerd, met een delay van 5 seconden en 0,5 seconden.
+  if(digitalRead(btn_1) == HIGH && digitalRead(btn_2) == HIGH){
+  	control_servo(5000, 500);
+  }
+  
+  // als de eerste knop zijn ingedrukt, wordt de 
+  // control servo functie uitgevoerd, 
+  // met een delay van 5 seconden en 5 seconden.
+  else if(digitalRead(btn_1) == HIGH) {
+    control_servo(5000, 5000);
+    Serial.println(current_angle);
+  }
+  
+  // als de tweede knop zijn ingedrukt, wordt de 
+  // control servo functie uitgevoerd, 
+  // met een delay van 0,5 seconden en 0,5 seconden.
+  else if(digitalRead(btn_2) == HIGH) {
+    control_servo(500, 500);
+  }
+}
+
+void control_servo(int ms1, int ms2){
+  // wissel de target angle een van de waarde van de target angles.
+  // 0 en 120.
+  if(target_angle == current_angle) {
+    // tel de teller 1 omhoog zodat target angle kan veranderen.
+    ta_counter ++;
+    
+    // zet target angle als een van de 2 taget angles.
+    target_angle = target_angles[ta_counter % 2];
+  }
+  
+  // draai naar rechts zolang de current angle minder is 
+  // dan de target angle.
+  else if(target_angle > current_angle){
+    // draai de servo naar rechts.
+    current_angle++;
+    
+    // stuur de servo aan met de current angle.
+    servo.write(current_angle);
+    
+    // bereken een delay gebaseerd op de eerste milliseconde waarde
+    // gedeeld door de tweede van de target angles.
+    delay(ms1 / target_angles[1]);
+  }
+    
+  // draai naar links zolang de current angle meer is 
+  // dan de target angle.
+  else{
+    // draai de servo naar links.
+    current_angle--;
+    
+    // stuur de servo aan met de current angle.
+    servo.write(current_angle);
+    
+    // bereken een delay gebaseerd op de tweede milliseconde waarde
+    // gedeeld door de tweede van de target angles.
+    delay(ms2 / target_angles[1]);
+  }
+}
